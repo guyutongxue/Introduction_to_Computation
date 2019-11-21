faa7192d8e52909f906d9aaf02b8b41377cb0f93
Guyutongxue <guyutongxue@163.com>
2019-11-21 10:51:11 +0800

    my solutiono updated

--- 20191120_P3_Practice3/8.BasicExpr.cpp
+++ 20191120_P3_Practice3/8.BasicExpr.cpp
@@ -1,88 +1,81 @@
 #include<iostream>
-#include<vector>
-#include<string>
-#include<cstring>
-#define INF 1<<30
-template<class T>
-class Calc {
-    static const int MX = 100000;
-    char op[MX];
-    int lch[MX], rch[MX], r ;
-    T s[MX];
-    bool DIV_ZERO;
-    int _build(const std::string& S, int L, int R) {
-        int u;
-        int t = L;
-        while (t <= R && (isdigit(S.at(t)) || S.at(t) == '.')) t++;
-        if (t == R + 1) {
-            u = r++;
-            op[u] = '.';
-            lch[u] = rch[u] = -1;
-            s[u] = std::atof(S.substr(L, t - L).c_str());
-            return u;
-        }
-        std::vector<int> c ;
-        c.push_back(-1);
-        c.push_back(-1);
-        for (int i = L, p = 0; i <= R; i++) {
-            if (S.at(i) == '(') p++;
-            else if (S.at(i) == ')') p--;
-            else if ((S.at(i) == '+' || S.at(i) == '-') && !p) c.at(0) = i;
-            else if ((S.at(i) == '*' || S.at(i) == '/') && !p) c.at(1) = i;
-        }
-        if (c.at(0) < 0)
-            c.at(0) = c.at(1);
-        if (c.at(0) < 0)
-            u = _build(S, L + 1, R - 1);
-        else {
-            u = r++;
-            op[u] = S[c[0]];
-            lch[u] = _build(S, L, c[0] - 1);
-            rch[u] = _build(S, c[0] + 1, R);
-            s[u] = -1;
-        }
-        return u;
+#include<sstream>
+#include<stack>
+#include<algorithm>
+using namespace std;
+stringstream ss;
+inline int getPrior(char x){
+    if(x=='*'||x=='/')return 2;
+    else return 1;
+}
+inline int calc(char p,int x,int y){
+    switch(p){
+        case '+':return x+y;
+        case '-':return x-y;
+        case '*':return x*y;
+        case '/':return x/y;
     }
-    T _calc(int u) {
-        if (op[u] == '.') return s[u];
-        T al = _calc(lch[u]), ar = _calc(rch[u]);
-        switch (op[u]) {
-        case '+': return al + ar;
-        case '-': return al - ar;
-        case '*': return al * ar;
-        case '/': return (ar == 0 ? DIV_ZERO = 1 : al / ar);
+}
+int parse(){
+    string c;
+    stack<int> b;
+    stack<char> s;
+    while(ss>>c){
+        if(c=="(")break;
+        else if(c==")")b.push(parse());
+        else if(c=="+"||c=="-"||c=="*"||c=="/"){
+            while(!s.empty()&&getPrior(c[0])<getPrior(s.top())){
+                int a1=b.top();b.pop();
+                int a2=b.top();b.pop();
+                b.push(calc(s.top(),a1,a2));
+                s.pop();
+            }
+            s.push(c[0]);
         }
-        //assert(false);
-        return al;
+        else b.push(atoi(c.c_str()));
     }
-public:
-    T solve(const std::string& s) {
-        r = 0;
-        DIV_ZERO = false;
-        int u = _build(s, 0, (int)s.length() - 1);
-        if (!DIV_ZERO)
-            return _calc(u);
-        else
-            /// 运算过程中发生除0
-            return INF;
+    while(!s.empty()){
+        int a1=b.top();b.pop();
+        int a2=b.top();b.pop();
+        b.push(calc(s.top(),a1,a2));
+        s.pop();
     }
-};
-Calc<long long> int_calc;
-Calc<long double> dbl_calc;
-
-int main() {
-    std::string expr;
-    while (std::cin >> expr) {
-       
-        
-        if (expr.find(".") == std::string::npos) {
-            long long ans = int_calc.solve(expr);
-            printf("%lld\n", ans);
+    return b.top();
+}
+int main(){
+    string a;
+    while(getline(cin,a)){
+        if(a[0]=='-'||a[0]=='+'){
+            a='0'+a;
         }
-        else {
-            long double ans = dbl_calc.solve(expr);
-            printf("%.6f\n", (double)ans);
+        for(int i=1;i<a.length();i++){
+            if(a[i-1]<'0'||a[i-1]>'9'||a[i]<'0'||a[i]>'9'){
+                if((a[i-1]=='(')&&(a[i]=='-'||a[i]=='+')){
+                    a.insert(i,1,'0');
+                    a.insert(i,1,' ');
+                    i+=2;
+                }
+                a.insert(i,1,' ');
+                i++;
+            }
         }
+        //cout<<a<<endl;
+        ss<<a;
+        string temp;
+        stack<string> haha;
+        while(ss>>temp)
+            haha.push(temp);
+        ss.clear();
+        a.clear();
+        while(!haha.empty()){
+            a+=haha.top();
+            a+=" ";
+            haha.pop();
+        }
+        //cout<<a<<endl;
+        ss<<a;
+        cout<<parse()<<endl;
+        ss.clear();
     }
     return 0;
 }
