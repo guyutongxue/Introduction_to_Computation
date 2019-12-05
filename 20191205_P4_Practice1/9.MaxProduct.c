c51ed4d868b1f5719c6c1143b7a56ca88430f969
Guyutongxue <guyutongxue@163.com>
2019-12-05 23:15:39 +0800

    a-geek-version!

---
+++ 20191205_P4_Practice1/9.MaxProduct.c
@@ -0,0 +1,5 @@
+s=0,n,i=2,q,j=2;
+main(){
+    for(scanf("%d",&n);s+i<=n;s+=i++);
+    for(i--;j++<=i;printf("%d%c",j+((q=i-n+s)?-(j<=-~q):j>i)," \n"[j>i]));
+}
--- 20191205_P4_Practice1/9.MaxProduct.cpp
+++ 20191205_P4_Practice1/9.MaxProduct.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int s=0,n,i=2,q,j;
-    cin>>n;
-    for(;s+i<=n;)s+=i++;
-    if(q=--i-n+s)for(j=2;j<=i;j++)cout<<j+(j>q)<<" \n"[j==i];
-    else{
-        for(int j=3;j<=i;j++)cout<<j<<" ";
-        cout<<i+2<<'\n';
-    }
+    int s=0,n,i=2,q,j=2;
+    for(cin>>n;s+i<=n;s+=i++);
+    for(i--;j++<=i;cout<<j+((q=i-n+s)?-(j<=-~q):j>i)<<" \n"[j>i]);
 }
